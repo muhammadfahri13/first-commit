2a57609a8359c3287e32abc90c1ca55e9b6a07d3
muhammadfahri13 <ysenpai785 @gmail.com>
2025-09-19 10:31:41 +0700

    7_boolean.dart

---
+++ 7_boolean.dart
@@ -0,0 +1,11 @@
+void main(List<String> args) {
+  // tipe data boolean
+  // simbol ! menandakan not atau bukan
+  // tipe data boolean hanya memiliki dua nilai true dan false
+
+  var isTrue = true;
+
+  print(isTrue); // true
+  print(!isTrue); // false
+  print(true !=isTrue); // false
+}
